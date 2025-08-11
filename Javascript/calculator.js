32e4d7bb6c0d2df9072c77c827cfca7920d1b17a
8164Smit <8164smitvasava@gmail.com>
2025-08-11 11:27:25 +0530

    Update calculator.js

--- Javascript/calculator.js
+++ Javascript/calculator.js
@@ -2,11 +2,11 @@ let num1 = 34;
 let num2 = 45;
 let operator = "+";
 
-let result = operator == "+" ? num1 + num2 :
-             operator == "-" ? num1 - num2 :
-             operator == "*" ? num1 * num2 :
-             operator == "/" ? num1 / num2 :
-             operator == "%" ? num1 % num2 :
-             "Invalid Operator";
+let result = operator === "+" ? num1 + num2 :
+             operator === "-" ? num1 - num2 :
+             operator === "*" ? num1 * num2 :
+             operator === "/" ? (num2 !== 0 ? num1 / num2 : "Error: Division by zero") :
+             operator === "%" ? (num2 !== 0 ? num1 % num2 : "Error: Modulo by zero") :
+             "Error: Invalid Operator";
 
-console.log("Result : ", result);
+console.log("Result:", result);
